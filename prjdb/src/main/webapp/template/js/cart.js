1e387b62d891fb40e9356852e12d13018dc84f6a
재은 <141694818+wnpdms@users.noreply.github.com>
2023-10-11 22:14:39 +0900

    1011 자기전..

--- prjdb/src/main/webapp/template/js/cart.js
+++ prjdb/src/main/webapp/template/js/cart.js
@@ -10,7 +10,7 @@ function number_format(amount) {
 }
 
 // prototype에 정의해서 메소드 추가: 숫자 3자리 콤마찍기
-Number.prototype.formatNumber = function () {
+Number.prototype.formatNumber = function() {
 	if (this == 0) return 0;
 	let regex = /(^[+-]?\d+)(\d{3})/;
 	let nstr = (this + '');
@@ -25,84 +25,122 @@ console((100000).formatNumber());
 let basket = {
 	cartCount: 0,
 	cartTotal: 0,
-	delCheckedItem: function () {
-	// 선택된 상품을 삭제....금액을 재계산.
+	delCheckedItem: function() {
+		// 선택된 상품을 삭제....금액을 재계산.
+		let ckbox = []
+		ckbox = document.querySelectorAll('input[type="checkbox"]:checked');
+		ckbox.forEach((item) => {
+			item.closest('div.row.data').remove();
+		})
+		basket.reCalc();
 	},
-	delAllItem: function () {
-	// 장바구니 비우기 하면 실행되도록..
+	delAllItem: function() {
+		// 장바구니 비우기 하면 실행되도록..
+		let cartList = document.querySelectorAll('div.row.template');
+		cartList.forEach((item) => {
+			item.remove();
+		})
+		basket.reCalc();
 	},
-	reCalc: function () {
-	// 금액을 재계산.
-	
-	// 수량
-	document.querySelector('div.sumcount').innerText = "상품갯수: " + cartItems.length+"개";
-	// 금액
-	let summoney = 0;
-	cartItems.forEach((item,idx)=>{
-		summoney += item.price*item.qty;
-	})
-	document.querySelector('div.summoney').onnerText = "합계금액: "+(summoney).formatNumber()+"원";
+	reCalc: function() {
+		// 금액을 재계산.
+
+		// 수량
+		let pNumArr = document.getElementsByClassName('p_num');
+		let count = 0;
+		for (let i = 0; i < pNumArr.length; i++) {
+			count += Number(pNumArr[i].getAttribute('value'));
+		}
+		// 금액
+		let pSumArr = [];
+		pSumArr = document.getElementsByClassName('sum');
+		let summoney = 0;
+		for (let i = 1; i < pSumArr.length; i++) {
+			summoney += Number(pSumArr[i].getAttribute('value'));
+		}
+		//
+		document.querySelector('div.summoney').onnerText = "합계금액: " + (summoney).formatNumber() + "원";
 	},
-	updateUI: function () {
-	// 화면을 refresh.
+	updateUI: function() {
+		// 화면을 refresh.
 		location.reload();
 	},
-	changePNum: function () {
-	// 수량변경.
+	changePNum: function(e) {
+		// 수량변경.
+		let val = parseInt(e.parentElement.childNodes[1].getAttribute('value'));
+		//up
+		if (e.childNodes[1].className.search("up") != -1) {
+			e.parentElement.childNodes[1].setAttribute('value', val + 1);
+			let price = Number(e.parentElement.parentElement.parentElement.childNodes[1].childNodes[1].getAttribute('value'));
+			e.parentElement.parentElement.parentElement.childNodes[5].setAttribute('value', price * (val + 1));
+			e.parentElement.parentElement.parentElement.childNodes[5].innerText = (price * (val + 1)).formatNumber() + "원";
+		} else if (e.childNodes[1].className.search("down") != -1 && val > 0) {
+			//down
+			//console.log("down");
+			e.parentElement.childNodes[1].setAttribute('value', val - 1);
+			let price = Number(e.parentElement.parentElement.parentElement.childNodes[1].childNodes[1].getAttribute('value'));
+			e.parentElement.parentElement.parentElement.childNodes[5].setAttribute('value', price * (val - 1));
+			e.parentElement.parentElement.parentElement.childNodes[5].innerText = (price * (val - 1)).formatNumber() + "원";
+		}
+		basket.reCalc();
 	},
-	delItem: function () {
-	// 삭제버튼 클릭시.
-	e.parentElement.parentElement.parentElement.remove();
+	delItem: function(e) {
+		// 삭제버튼 클릭시.
+		e.closest('div.row.data').remove();
+		basket.reCalc();
 	},
-	cartList: function () {
-	// 상품목록 출력...아래에 있는 상품정보를 활용해서 수량만큼 출력이 되도록.
-		cartItems.forEach((item, idx) => {
+	cartList: function() {
+		// 상품목록 출력...아래에 있는 상품정보를 활용해서 수량만큼 출력이 되도록.
+		cartItems.forEach((item) => {
 			let template = document.querySelector('div.row.data').cloneNode(true);
 			// 이미지
-			template.childNodes[1].childNodes[3].firstChild.setAttribute('src','./img/'+item.image);
-			// 제품명
-			template.childNodes[1].childNodes[5].childNodes[1].innerText = item.productNm;
-			// 가격
-			template.childNodes[3].childNodes[1].firstChild.setAttribute('id','p_price'+item.no);
-			template.childNodes[3].childNodes[1].firstChild.setAttribute('value',item.price);
-			template.childNodes[3].childNodes[1].innerText = (item.price).formatNumber()+"원";
-			// 수량(id,name,value,button)
-			template.childNodes[3].childNodes[3].childNodes[1].childNodes[1].setAttribute('id','p_num'+item.no);
-			template.childNodes[3].childNodes[3].childNodes[1].childNodes[1].setAttribute('name','p_num'+item.no);
-			template.childNodes[3].childNodes[3].childNodes[1].childNodes[1].setAttribute('value',item.qty);
-			template.childNodes[3].childNodes[3].childNodes[1].childNodes[3].setAttribute('onclick','basket.changePNum()');
-			template.childNodes[3].childNodes[3].childNodes[1].childNodes[5].setAttribute('onclick','basket.changePNum()');
-			// 합계
-			template.childNodes[3].childNodes[5].innerText = (item.price*item.qty).formatNumber()+"원";
-			// 삭제버튼(함수)
-			template.childNodes[5].childNodes[1].childNodes[1].setAttribute('onclick','basket.delItem(this)');
+			console.log(template.querySelector('input[name="p_price"]').nextSibling);
+			template.querySelector('img').setAttribute('src', './img/' + item.image);
+
+			//제품명
+			template.querySelector('.pname').querySelector('span').innerText = item.productNm;
+
+			//가격(id,value,text)
+			template.querySelector('input[name="p_price"]').setAttribute('id', 'p_price' + item.no);
+			template.querySelector('input[name="p_price"]').setAttribute('value', item.price);
+			template.querySelector('input[name="p_price"]').nextSibling.textContent = (item.price).formatNumber() + "원";
+
+			//수량(id,name,value,button)
+			template.querySelector('.p_num').setAttribute('id', 'p_num' + item.no);
+			template.querySelector('.p_num').setAttribute('name', 'p_num' + item.no);
+			template.querySelector('.p_num').setAttribute('value', item.qty);
+
+			//합계
+			template.childNodes[3].childNodes[5].innerText = (item.price * item.qty).formatNumber() + "원";
+			template.childNodes[3].childNodes[5].setAttribute('value', item.price * item.qty);
 			document.querySelector('div.basketdiv').append(template);
+
 		})
-			basket.reCalc();
+		basket.reCalc();
 	}
 };
 
 var cartItems = [{
-		no: 1,
-		productNm: '장바구니1',
-		qty: 2,
-		price: 12000,
-		image: 'basket1.jpg'
-	},
-	{
-		no: 2,
-		productNm: '장바구니2',
-		qty: 1,
-		price: 22000,
-		image: 'basket2.jpg'
-	},
-	{
-		no: 3,
-		productNm: '고급장바구니',
-		qty: 1,
-		price: 23600,
-		image: 'basket3.jpg'
-	}
+	no: 1,
+	productNm: '장바구니1',
+	qty: 2,
+	price: 12000,
+	image: 'basket1.jpg'
+},
+{
+	no: 2,
+	productNm: '장바구니2',
+	qty: 1,
+	price: 22000,
+	image: 'basket2.jpg'
+},
+{
+	no: 3,
+	productNm: '고급장바구니',
+	qty: 1,
+	price: 23600,
+	image: 'basket3.jpg'
+}
 ]
 
 basket.cartList();
